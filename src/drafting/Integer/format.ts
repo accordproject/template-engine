ebd8a550f3a8c2dbd004c51e475277c1ecedd258
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-03-12 08:31:41 +0200

    feat(Integer): add format to words
    Signed-off-by: ahmad-kashkoush <ahmedkashkoush464@gmail.com>

--- src/drafting/Integer/format.ts
+++ src/drafting/Integer/format.ts
@@ -12,7 +12,7 @@
  * limitations under the License.
  */
 
-import { numberToText } from "./numberToTextFormatter";
+import {ToWords} from 'to-words';
 
 /**
  * Creates a drafter for Integer
@@ -30,18 +30,20 @@ export function draftInteger(value:number) : string {
  * @returns {object} the parser
  */
 export function draftIntegerFormat(value:number,format:string) : string {
-  if (format === "text") {
-    return numberToText(value);
-  } else {
-    return format.replace(/0(.)0/gi, function(_a,sep1){
-        const vs = value.toFixed(0);
-        let res = '';
-        let i = vs.substring(0,vs.length);
-        while (i.length > 3) {
-            res = sep1 + i.substring(i.length - 3) + res;
-            i = i.substring(0, i.length - 3);
-        }
-        return i + res;
-    });
-  }
+    if (format === 'word') {
+        const converter:ToWords = new ToWords();
+        const res:string=converter.convert(value);
+        return res;
+    } else {
+        return format.replace(/0(.)0/gi, function(_a,sep1){
+            const vs = value.toFixed(0);
+            let res = '';
+            let i = vs.substring(0,vs.length);
+            while (i.length > 3) {
+                res = sep1 + i.substring(i.length - 3) + res;
+                i = i.substring(0, i.length - 3);
+            }
+            return i + res;
+        });
+    }
 }
