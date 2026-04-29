32cb8e694f8f66a0068bbf020b3949f15e8d28a4
Devanshi Chhatbar <117909426+devanshi00@users.noreply.github.com>
2026-04-29 10:45:23 +0530

    feat: LLM based template logic executor
    Signed-off-by: Devanshi Chhatbar <117909426+devanshi00@users.noreply.github.com>

---
+++ perishable-goods/README.md
@@ -0,0 +1,3 @@
+# Perishable Goods 
+
+This clause specifies penalties if the transport conditions (temperature and humidity) for a package are breached.
---
+++ perishable-goods/logic/generated/concerto@1.0.0.ts
@@ -0,0 +1,86 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+// Generated code for namespace: concerto@1.0.0
+
+// imports
+
+// Warning: Beware of circular dependencies when modifying these imports
+import type {
+	IPerishableGoodsState,
+	ISensorReading,
+	IShipment
+} from './io.clause.perishablegoods@0.1.0';
+import type {
+	Month,
+	Day,
+	TemporalUnit,
+	IDuration,
+	PeriodUnit,
+	IPeriod
+} from './org.accordproject.time@0.3.0';
+import type {
+	IDigitalMonetaryAmount,
+	DigitalCurrencyCode,
+	IMonetaryAmount,
+	CurrencyCode,
+	ICurrencyConversion
+} from './org.accordproject.money@0.3.0';
+
+// Warning: Beware of circular dependencies when modifying these imports
+import type {
+	IContract,
+	IClause
+} from './org.accordproject.contract@0.2.0';
+import type {
+	IState
+} from './org.accordproject.runtime@0.2.0';
+
+// Warning: Beware of circular dependencies when modifying these imports
+import type {
+	IRequest,
+	IResponse
+} from './org.accordproject.runtime@0.2.0';
+
+// Warning: Beware of circular dependencies when modifying these imports
+import type {
+	IObligation
+} from './org.accordproject.runtime@0.2.0';
+
+// interfaces
+export interface IConcept {
+   $class: string;
+}
+
+export type ConceptUnion = IPerishableGoodsState | 
+ISensorReading | 
+IShipment | 
+IDuration | 
+IPeriod | 
+IDigitalMonetaryAmount | 
+IMonetaryAmount | 
+ICurrencyConversion;
+
+export interface IAsset extends IConcept {
+   $identifier: string;
+}
+
+export type AssetUnion = IContract | 
+IClause | 
+IState;
+
+export interface IParticipant extends IConcept {
+   $identifier: string;
+}
+
+export interface ITransaction extends IConcept {
+   $timestamp: Date;
+}
+
+export type TransactionUnion = IRequest | 
+IResponse;
+
+export interface IEvent extends IConcept {
+   $timestamp: Date;
+}
+
+export type EventUnion = IObligation;
+
