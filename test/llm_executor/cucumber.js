e5efdaf18aefbf2e556e3527c9af1c7d42c3e3be
“Devanshi <117909426+devanshi00@users.noreply.github.com>
2026-08-14 14:19:37 +0530

    add llm test framework
    Signed-off-by: “Devanshi <117909426+devanshi00@users.noreply.github.com>

--- test/llm_executor/cucumber.js
+++ test/llm_executor/cucumber.js
@@ -3,7 +3,7 @@
 module.exports = {
   default: {
     paths: ["*.feature"],
-    require: ["single-stateless-template.steps.js"],
+    require: ["single-stateless-template_steps.js"],
     format: ["progress-bar", "html:reports/cucumber-report.html"],
     publishQuiet: true,
     timeout: 120000,
